b8753c9c9f1a8357aac28885e474e555b6990b07
Yerlan Ismailov <y.m.ismailov@gmail.com>
2018-01-31 23:13:54 +0600

    REST controller for async quiz

---
+++ src/main/java/kz/iitu/SuperEyesApp/rest_controller/TestRestController.java
@@ -0,0 +1,31 @@
+package kz.iitu.SuperEyesApp.rest_controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import kz.iitu.SuperEyesApp.model.Test;
+import kz.iitu.SuperEyesApp.repository.TestRepository;
+
+@RestController
+@RequestMapping("/api/tests")
+public class TestRestController {
+	
+	@Autowired
+	private TestRepository testRepo;
+	
+	@GetMapping
+	public ResponseEntity<List<Test>>getAllTests() {
+		List<Test>tests = new ArrayList<>();
+		tests = testRepo.findAll();
+		
+		return new ResponseEntity<List<Test>>(tests, HttpStatus.OK);
+	}
+	
+}
